06936b2af782c0cdcd0d79e072a9cb344fb10e27
NrA <121941131+nofil-r-akhtar@users.noreply.github.com>
2026-02-22 19:27:23 +0500

    New Fixes for Vercel Deployment

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "609864838"
+    serviceWorkerVersion: "1854324691"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e3f305626a7d819395c487aa2d666916
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "61b5521a9df919cd679b91bace424681",
+"flutter_bootstrap.js": "c2fd1a67623e81b5ab2ac610f72c1898",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
