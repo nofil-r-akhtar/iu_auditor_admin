34d96aee0475581c2c8ee7d1fd53fe7195866eb2
NrA <121941131+nofil-r-akhtar@users.noreply.github.com>
2026-04-14 11:08:48 +0500

    Updated Release - 14/4/26

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e3f305626a7d819395c487aa2d666916
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "8dc1eef6a9c4a8ee13a8c1d0b248f482",
+"flutter_bootstrap.js": "eff3228cc185209f9335e348788adef7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
