f417a8d2b747d3afbc3e82f25fb3286fa2025045
NrA <121941131+nofil-r-akhtar@users.noreply.github.com>
2026-04-27 10:05:46 +0500

    Header

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1612667536"
+    serviceWorkerVersion: "4195206307"
   }
 });
